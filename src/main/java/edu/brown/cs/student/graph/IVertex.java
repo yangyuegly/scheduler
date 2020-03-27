688b5347582879e798548c0bc7dffe148a1f012b
Yue Yang <yangyuegly@gmail.com>
2020-03-27 09:46:12 -0400

    graph structure

--- src/main/java/edu/brown/cs/student/graph/IVertex.java
+++ src/main/java/edu/brown/cs/student/graph/IVertex.java
@@ -1,5 +1,65 @@
 package edu.brown.cs.student.graph;
+import java.util.Set;
 
-public interface IVertex {
+/**
+ * Interface that represents a vertex in the graph.
+ * @param <V> a vertex
+ * @param <E> an edge
+ */
+public interface IVertex<V, E> {
 
+  /**
+   * gets the incident edges of the vertex.
+   * @return a set of incident edges
+   */
+  Set<E> getIncidentEdges();
+
+  /**
+   * adds incident edges to a given vertex.
+   * @param list list of edges to add to the incident edges
+   */
+  void addIncidentEdges(Set<E> list);
+
+  /**
+   * Gets the cost of the getting to the vertex.
+   * @return the cost of the vertex
+   */
+  Double getCost();
+
+  /**
+   * Sets the cost of getting to the vertex.
+   * @param cost the cost that we want to set the vertex to
+   */
+  void setCost(Double cost);
+
+  /**
+   * Sets the used boolean to b.
+   * @param b the value we want to set the used boolean to
+   */
+  void setUsed(boolean b);
+
+  /**
+   * Gets the value of the used boolean.
+   * @return the used boolean
+   */
+  boolean getUsed();
+
+  /**
+   * Sets the previous edge that the vertex is connected to.
+   * @param pathPrev the previous edge we want to set
+   */
+  void setPrev(E pathPrev);
+
+  /**
+   * Gets the previous edge of the vertex.
+   * @return the previous edge as type E
+   */
+  E getPrev();
+
+  /**
+   * Gets the id of the vertex.
+   * @return the id of the vertex as a string
+   */
+  String getID();
 }
+
--- src/main/java/edu/brown/cs/student/graph/UndirectedWeightedGraph.java
+++ src/main/java/edu/brown/cs/student/graph/UndirectedWeightedGraph.java
@@ -1,5 +1,55 @@
 package edu.brown.cs.student.graph;
 
-public class UndirectedWeightedGraph {
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
+import edu.brown.cs.student.database.Database;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+/**
+ * The Graph class takes in a V that extends Vertex and E that extends
+ * Edge. It gets information from a DataStore and runs a dynamic
+ * dikstra's algorithm to find the shortest path between to nodes in the graph.
+ * @param <V> an object that implements Vertex
+ * @param <E> an object that implements Edge
+ */
+public class UndirectedWeightedGraph<V extends IVertex<V, E>, E extends IEdge<V, E>> {
+  // edge table keeps track of weights
+  private Database db;
+  private HashSet<E> graph;
+
+  /**
+   * Constructor for the graph. It takes in a datastore and instantiates a hashset that
+   * represents a graph.
+   * @param db a datastore
+   */
+  public UndirectedWeightedGraph(Database db) {
+    // two nodes may be connected iff 1) they were in the same movie 2) they share
+    // initial
+    this.graph = new HashSet<E>();
+    this.db = db;
+
+  }
+
+  /**
+   * Get edges between two nodes.
+   *
+   * @param a the vertex that you want to get the edges for
+   * @return a list of incident edges
+   */
+  public Set<E> getAllEdges(V a) {
+    return a.getIncidentEdges();
+  }
+
+ 
 }
+
+
+

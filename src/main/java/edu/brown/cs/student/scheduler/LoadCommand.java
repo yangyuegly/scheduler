632adf05cfcbc01134aee9c5922d2a2a441337b9
Yue Yang <yangyuegly@gmail.com>
2020-04-17 13:40:26 -0400

    added structure to scheduling comment

--- src/main/java/edu/brown/cs/student/scheduler/LoadCommand.java
+++ src/main/java/edu/brown/cs/student/scheduler/LoadCommand.java
@@ -65,12 +65,8 @@ import com.mongodb.client.result.UpdateResult;
 // (attendees, event1, event2, event3)
 // store the csv title and user id into the user collection
 public class LoadCommand {
-  UndirectedWeightedGraph<Event, Conflict> g = null;
-  List<Event> node = new ArrayList<>();
-  Set<Conflict> edges = new HashSet<>();
-  public LoadCommand(UndirectedWeightedGraph<Event, Conflict> g) {
-    this.g = g;
-    this.node = new ArrayList<Event>();
+
+  public LoadCommand() {
 
   }
 
@@ -99,13 +95,20 @@ public class LoadCommand {
             nameToId.put(second, count);
             count++;
           }
-          Conflict conflict = new Conflict(new Event(nameToId.get(first), first), new Event(nameToId.get(second),second), null);
+          Conflict conflict = new Conflict(new Event(nameToId.get(first), first),
+              new Event(nameToId.get(second), second), null);
           //add to the weight if the conflict doesn't exist or add the conflict itself
           frequencyMap.put(conflict, frequencyMap.getOrDefault(conflict, 0) + 1);
         }
       }
 
     }
+
+    //fill the vertices 
+    for (Map.Entry<String, Integer> entry : nameToId.entrySet()) {
+      nodes.add(new Event(entry.getValue(), entry.getKey()));
+    }
+
 
     Document nestDoc = new Document("convention_id", conventionID).append("conflicts",
         Arrays.asList());
@@ -116,6 +119,7 @@ public class LoadCommand {
       //set weight for each conflict
       entry.getKey().setWeight(entry.getValue());
       edges.add(entry.getKey());
+      
       BasicDBObject query = new BasicDBObject();
       query.put("convention_id", conventionID);
 
--- src/main/java/edu/brown/cs/student/scheduler/ScheduleCommand.java
+++ src/main/java/edu/brown/cs/student/scheduler/ScheduleCommand.java
@@ -1,5 +1,11 @@
 package edu.brown.cs.student.scheduler;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import edu.brown.cs.student.graph.UndirectedWeightedGraph;
 import edu.brown.cs.student.main.ICommand;
 
 /**
@@ -7,7 +13,10 @@ import edu.brown.cs.student.main.ICommand;
  * ICommand interface.
  */
 public class ScheduleCommand implements ICommand {
-
+  UndirectedWeightedGraph<Event, Conflict> g = null;
+  List<Event> nodes = new ArrayList<>();
+  Set<Conflict> edges = new HashSet<>();
+  
   @Override
   public String getKeyword() {
     // TODO Auto-generated method stub

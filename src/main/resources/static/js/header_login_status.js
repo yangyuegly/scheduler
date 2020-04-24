596e8a2f4a95e1303645b5a5ac433a2c71eeb9d6
Abby Goldberg <abbygoldberg@Abby-MacBook-Pro.local>
2020-04-23 21:16:04 -0400

    worked on user dropdown; currently the dropdown won't appear

--- src/main/resources/static/js/header_login_status.js
+++ src/main/resources/static/js/header_login_status.js
@@ -1,28 +1,55 @@
-const $loginStatusLink = $("#loginStatusLink");
-const $logoutLink = $("#logoutLink");
+// const $loginStatusLink = $("#loginStatusLink");
+// const $logoutLink = $("#logoutLink");
 
 /*
   When the document is ready, this runs.  It sets up the part of the header
   describing whether or not the user is logged in.
 */
 // $(document).ready(() => {
-  const cookie = document.cookie;
-
-  if (cookie == "") {
-    // the user is not logged in
-    $('#notLoggedInDiv').css("display", "block");
-    $('#loggedInDiv').css("display", "none");
-  } else {
-    const splitCookie = cookie.split("=")
-    const userEmail = splitCookie[1];
-    $('#loggedInDiv').css("display", "block");
-    $('#notLoggedInDiv').css("display", "none");
-    $loginStatusLink.text(userEmail);
-  }
+  // const cookie = document.cookie;
+  //
+  // if (cookie == "") {
+  //   // the user is not logged in
+  //   $('#notLoggedInDiv').css("display", "block");
+  //   $('#loggedInDiv').css("display", "none");
+  // } else {
+  //   const splitCookie = cookie.split("=")
+  //   const userEmail = splitCookie[1];
+  //   $('#loggedInDiv').css("display", "block");
+  //   $('#notLoggedInDiv').css("display", "none");
+  //   $loginStatusLink.text(userEmail);
+  // }
 // });
 
-// $logoutLink.click(function() {
-//   // the user is logging out
-//   $('#notLoggedInDiv').css("display", "block");
-//   $('#loggedInDiv').css("display", "none");
+
+const $loginHeaderLink = $("#loginHeaderLink");
+const $logoutHeaderLink = $("#logoutHeaderLink");
+const $userDropdown = $("#userDropdown");
+const $userDropdownContent = $("#userDropdownContent");
+const $userHeaderLink = $("#userHeaderLink");
+
+const cookie = document.cookie;
+
+if (cookie == "") {
+  // the user is not logged in
+  $loginHeaderLink.css("display", "block");
+  //$logoutHeaderLink.css("display", "none");
+  $userDropdown.css("display", "none");
+} else {
+  const splitCookie = cookie.split("=")
+  const userEmail = splitCookie[1];
+
+  $logoutHeaderLink.css("display", "block");
+  $userDropdown.css("display", "block");
+  //$userEmailHeaderLink.text("Logged in as " + userEmail);
+  $loginHeaderLink.css("display", "none");
+}
+
+/*
+  Displays the drop-down menu when the user icon is clicked.  If it is clicked
+  while the drop-down is already displayed, the drop-down is hidden.
+*/
+// $userHeaderLink.hover(function() {
+//   $userDropdownContent.css("display", "block");
+//   //document.getElementById("userDropdownContent").classList.toggle("show");
 // });
